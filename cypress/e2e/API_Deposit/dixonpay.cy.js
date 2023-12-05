778067b785359bff660ab5b20afcd1ab325196aa
Jester Longakit <jester@orientalwallet.com>
2023-12-05 17:18:48 +0800

    Checking of computations and API | API and UI (pending)

--- cypress/e2e/API_Deposit/dixonpay.cy.js
+++ cypress/e2e/API_Deposit/dixonpay.cy.js
@@ -10,7 +10,7 @@ var transaction = 0
 let randomInt = getRandomInt(1, 2);
 let cardnoselector = ['5111111111111118', '3530111333300000']
 
-for (transaction; transaction < 5; transaction++) {
+for (transaction; transaction < 1; transaction++) {
 
 let PK = "pk_FHmnhpf8iX8CeZd385tt8RwoqYguL42G"
 let SK = "sk_WE5KIGte3SU8rnf3NrcBVYQIoY3Z9nJe"
@@ -54,7 +54,7 @@ describe('DixonPay', () => {
         }).as('details');
         cy.get('@details').its('status').should('eq', 201);
         cy.get('@details').then((response) => {
-            // check the response body
+            // [API] check the response body
             expect(response.body.data).to.have.property('transaction_number');
             expect(response.body.data).to.have.property('reference_no');
             expect(response.body.data).to.have.property('status');
@@ -74,43 +74,53 @@ describe('DixonPay', () => {
             const mdr_fee = (7.5 / 100)*credit_amount
             const fixed_fee = 1
             const total_fee = mdr_fee + fixed_fee
+            const rounded_total_fee = parseFloat(total_fee.toFixed(2))
             const total_amount_comp = credit_amount - total_fee
+            const rounded_amount_comp = parseFloat(total_amount_comp.toFixed(2))
             
-            // check the Settlement Details in API response
-            expect(fee).to.eq(total_fee);
-            expect(total_amount).to.eq(total_amount_comp);
+            // [API] check the Settlement Details in API response
+            expect(fee).to.eq(rounded_total_fee);
+            expect(total_amount).to.eq(rounded_amount_comp);
 
-            // check ref.no and transaction status in API response
+            // [API] check ref.no and transaction status in API response
             expect(response.body.data.reference_no).to.eq(ref_no);
             expect(response.body.data.status).to.eq("pending");
             
-            // check Transaction Details
+            // [UI] check Transaction Details
             cy.visit(common.login_url);
             cy.get(loginpage_locators.email_field).type(common.adminEmail)
             cy.get(loginpage_locators.pass_field).type(common.adminPass)
             cy.get(loginpage_locators.submit_button).click()
             cy.wait(2000)
             cy.visit("https://develop.paystage.net/"+ response.body.data.transaction_number +"/transactions")
-
-            //check the status in transaction details if transaction is pending
+            
+            // [UI] check the status in transaction details if transaction is pending
             cy.get('.gap-y-3 > :nth-child(2) > .list-value').contains('pending')
             cy.wait(2000)
+            
+            // [UI] check the Settlement Details in Transaction Details
+            let transactionDetailsSettlementAmount = ".gap-y-3 > :nth-child(2) > .flex"
+            let transactionDetailsSettlementFee = ":nth-child(4) > .flex"
+            let transactionDetailsSettlementTotalAmount = ":nth-child(6) > .flex"
+            
+            cy.get(transactionDetailsSettlementAmount).should('have.text', 'USD '+credit_amount + '.00');
+            cy.get(transactionDetailsSettlementFee).should('have.text', ' USD '+rounded_total_fee);
+            cy.get(transactionDetailsSettlementTotalAmount).should('have.text', 'USD '+rounded_amount_comp);
 
-            // check the Settlement Details in Transaction Details
-            // check ref.no and transaction status in Transaction Details
+            // [UI] check ref.no and transaction status in Transaction Details
+            
             
             // check if API response body is equal to Webhook response body
-            // check if Settlement Details in API response is equal to Settlement Details Transaction Details
             // check if ref.no and transaction status in API response is equal to ref.no and transaction status in Transaction Details
 
             // process the dixonpay transaction
-            cy.visit(response.body.data.checkout_url)
-            cy.get('#cardNumber').type(cardnoselector[randomInt])
-            cy.get('#accountName').type('dxntest')
-            cy.get('#cvv').type('111')
-            cy.get(':nth-child(4) > .form-control').select('05')
-            cy.get(':nth-child(5) > .form-control').select('2042')
-            cy.get('#btn-submit').click()
+            // cy.visit(response.body.data.checkout_url)
+            // cy.get('#cardNumber').type(cardnoselector[randomInt])
+            // cy.get('#accountName').type('dxntest')
+            // cy.get('#cvv').type('111')
+            // cy.get(':nth-child(4) > .form-control').select('05')
+            // cy.get(':nth-child(5) > .form-control').select('2042')
+            // cy.get('#btn-submit').click()
 
             // check ref.no and transaction status in API response
             // check Transaction Details
